b7a01dc1ffb15b4b735c3935b914894cee9a882b
xxx3105 <123968288+xxx3105@users.noreply.github.com>
2023-06-17 13:16:53 +0200

    1

---
+++ js/task-01.js
@@ -0,0 +1,64 @@
+
+//part1
+
+const categoriesCalc = document.querySelectorAll('h2');
+
+let countCat = 0;
+
+categoriesCalc.forEach(() => {
+    countCat++;
+});
+
+console.log(`Number of categories: ${countCat}`);
+
+//part2
+
+const nameOfCategory = document.querySelectorAll('.item');
+
+nameOfCategory.forEach(data => {
+        
+    console.log(`Category: ${data.firstElementChild.textContent}`)
+    console.log(`Elements: ${data.lastElementChild.children.length}`)
+});
+
+//////////////////////////////////////////////////////////////////
+
+// const y = nameAndCategory.querySelectorAll('h2')
+// console.log(y)
+
+
+// const x = nameAndCategory.querySelector('.item');
+// console.log(x.textContent)
+
+// Напиши скрипт который:
+
+//     // Посчитает и выведет в консоль количество категорий в ul#categories, то есть
+//     элементов li.item.
+//     // Для каждого элемента li.item в списке ul#categories, найдет и выведет
+//     в консоль текст заголовка элемента (тега < h2 >) и количество элементов
+//      в категории(всех вложенных в него < li >).
+// // Для выполнения этой задачи нужно использовать метод forEach() и свойства
+// навигации по DOM.
+
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+
+//opt1
+
+// const categoriesCalc = document.querySelectorAll('h2');
+
+// const categoriesCalcRes = categoriesCalc.length;
+
+// console.log(`Number of categories: ${categoriesCalcRes}`)
+
